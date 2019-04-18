51bd79b16869b353077066ef94753973f260f73c
kraju3 <35513942+kraju3@users.noreply.github.com>
2019-04-18 01:27:09 -0500

    BingoButton class

--- Bingo Game/src/Bingo.java
+++ Bingo Game/src/Bingo.java
@@ -1,2 +1,19 @@
+import javafx.scene.control.Button;
+
 public class Bingo {
+
+
+}
+
+class BingoButton extends Button {//extends button so it's easier for the GUI team to develop the board
+    private String number;//could possibly be an int too
+
+    BingoButton(String s) {
+        this.number = s;
+
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
 }
